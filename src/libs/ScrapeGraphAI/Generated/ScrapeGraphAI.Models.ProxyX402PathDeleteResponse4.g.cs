da5fb297f594e530ede7cf4351a1c1a3d0326282
Konstantin S. <havendv@gmail.com>
2026-05-18 19:32:46 +0400

    feat: Updated OpenAPI spec (#347)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/ScrapeGraphAI/Generated/ScrapeGraphAI.Models.ProxyX402PathDeleteResponse4.g.cs
+++ src/libs/ScrapeGraphAI/Generated/ScrapeGraphAI.Models.ProxyX402PathDeleteResponse4.g.cs
@@ -6,7 +6,7 @@ namespace ScrapeGraphAI
     /// <summary>
     /// 
     /// </summary>
-    public sealed partial class ProxyX402PathGetResponse4
+    public sealed partial class ProxyX402PathDeleteResponse4
     {
 
         /// <summary>
--- src/libs/ScrapeGraphAI/Generated/ScrapeGraphAI.Models.ScrapeRequestDB.g.cs
+++ src/libs/ScrapeGraphAI/Generated/ScrapeGraphAI.Models.ScrapeRequestDB.g.cs
@@ -67,7 +67,7 @@ namespace ScrapeGraphAI
         public global::System.DateTime? FinishedAt { get; set; }
 
         /// <summary>
-        /// Default Value: 2026-05-18T06:51:40.255108
+        /// Default Value: 2026-05-18T14:08:05.050394
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("requested_at")]
         public global::System.DateTime? RequestedAt { get; set; }
@@ -110,7 +110,7 @@ namespace ScrapeGraphAI
         /// <param name="error"></param>
         /// <param name="finishedAt"></param>
         /// <param name="requestedAt">
-        /// Default Value: 2026-05-18T06:51:40.255108
+        /// Default Value: 2026-05-18T14:08:05.050394
         /// </param>
         /// <param name="cost">
         /// Default Value: 0
