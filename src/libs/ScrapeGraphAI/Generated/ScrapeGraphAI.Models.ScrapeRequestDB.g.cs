2350cd26c1797d10a1b7686cef793150b050a4eb
Konstantin S. <havendv@gmail.com>
2026-07-15 23:10:16 +0400

    feat: Updated OpenAPI spec (#650)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/ScrapeGraphAI/Generated/ScrapeGraphAI.Models.ScrapeRequestDB.g.cs
+++ src/libs/ScrapeGraphAI/Generated/ScrapeGraphAI.Models.ScrapeRequestDB.g.cs
@@ -67,7 +67,7 @@ namespace ScrapeGraphAI
         public global::System.DateTime? FinishedAt { get; set; }
 
         /// <summary>
-        /// Default Value: 2026-07-15T12:21:30.026514
+        /// Default Value: 2026-07-15T15:00:03.632465
         /// </summary>
         [global::System.Text.Json.Serialization.JsonPropertyName("requested_at")]
         public global::System.DateTime? RequestedAt { get; set; }
@@ -110,7 +110,7 @@ namespace ScrapeGraphAI
         /// <param name="error"></param>
         /// <param name="finishedAt"></param>
         /// <param name="requestedAt">
-        /// Default Value: 2026-07-15T12:21:30.026514
+        /// Default Value: 2026-07-15T15:00:03.632465
         /// </param>
         /// <param name="cost">
         /// Default Value: 0
