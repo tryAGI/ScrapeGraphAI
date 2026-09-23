52ccaa9a723c7091cde6bcfe384742478833c234
Konstantin S. <havendv@gmail.com>
2026-09-23 14:52:17 +0400

    feat: Updated OpenAPI spec (#990)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/ScrapeGraphAI/Generated/ScrapeGraphAI.Models.ProxyX402PathGetResponse5.g.cs
+++ src/libs/ScrapeGraphAI/Generated/ScrapeGraphAI.Models.ProxyX402PathGetResponse5.g.cs
@@ -6,7 +6,7 @@ namespace ScrapeGraphAI
     /// <summary>
     ///
     /// </summary>
-    public sealed partial class ProxyX402PathPutResponse3
+    public sealed partial class ProxyX402PathGetResponse5
     {
 
         /// <summary>
